94af49279e628c44a9d976143716cce8ca4f64b7
kyrem1 <goingpiano@gmail.com>
2019-02-23 21:57:56 -0600

    Began Perceptron

---
+++ Perceptron/src/Perceptron.java
@@ -0,0 +1,17 @@
+/**
+ * @author kyrem1
+ * @version 1
+ * @since 02-23-19
+ */
+public class Perceptron {
+    private final int INPUT_SIZE;
+
+    public Perceptron(int inputs) {
+        this.INPUT_SIZE = inputs;
+    }
+
+    public float evaluate() {
+
+    }
+
+}
